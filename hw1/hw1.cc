6ec7d9c1fbc5e586f749e8c533841a4002434f7a
Joe <joe210025@gmail.com>
2021-10-24 02:06:14 +0800

    swap and compare all subarray between processees. And sort of part which is exchange at last stage. seconds = 195.11?

--- hw1/hw1.cc
+++ hw1/hw1.cc
@@ -3,11 +3,6 @@
 #include <stdlib.h>
 #include <algorithm>
 
-typedef struct Group_info {
-	MPI_Group group;
-	MPI_Comm comm;
-} Group_info;
-
 typedef struct Proc_info {
 	int rank;
 	int total_proc;		// total process in this program
@@ -17,8 +12,8 @@ typedef struct Proc_info {
 	int next_len;		// len of data in rank + 1
 	int first_index;
 	int last_index;
-	Group_info group_info;
-} Proc_info;
+	int valid;			// process dose not have any data to sort
+} proc_info;
 
 int get_proc_len(int rank, int total_proc, int total_data_len) {
 	if (rank < 0 || rank > total_proc - 1) {
@@ -33,60 +28,34 @@ int get_proc_len(int rank, int total_proc, int total_data_len) {
 	return len;
 }
 
-// if no data to assign to process return 1, else return 0.(success return 0)
-int init_group_info(Proc_info *proc_info, int rank, int total_proc, int total_data_len) {
-	int ret;
-	MPI_Group orig_group, new_group;
-	MPI_Comm new_comm;
-	MPI_Comm_group(MPI_COMM_WORLD, &orig_group);
+void init_proc_info(proc_info *info, int rank, int total_proc, int total_data_len) {
+	info->rank = rank;
+	info->total_proc = total_proc;
+	info->len = get_proc_len(rank, total_proc, total_data_len);
+	info->pre_len = get_proc_len(rank - 1, total_proc, total_data_len);
+	info->next_len = get_proc_len(rank + 1, total_proc, total_data_len);
 
-	if (total_data_len < total_proc) {
-		int *ranks = (int *)malloc(sizeof(int) * total_data_len);
-		for (int i = 0; i < total_data_len; i++) {
-			ranks[i] = i;
-		}
-
-		MPI_Group_incl(orig_group, total_data_len, ranks, &new_group);
-		MPI_Comm_create(MPI_COMM_WORLD, new_group, &new_comm);
-
-		
-		if (rank < total_data_len) {
-			proc_info->group_info.group = new_group;
-			proc_info->group_info.comm = new_comm;
-			ret = 0;
-		} else {
-			proc_info->group_info.group = orig_group;
-			proc_info->group_info.comm = MPI_COMM_WORLD;
-			ret = 1;
-		}
-	} else {
-		proc_info->group_info.group = orig_group;
-		proc_info->group_info.comm = MPI_COMM_WORLD;
-		ret = 0;
+	info->first_index = info->len * info->rank;
+	if (rank >= total_data_len % total_proc) {
+		info->first_index += total_data_len % total_proc;
 	}
-	return ret;
-}
-
-void init_proc_info(Proc_info *proc_info, int total_proc, int total_data_len) {
-	MPI_Comm_rank(proc_info->group_info.comm, &proc_info->rank);
-	MPI_Comm_size(proc_info->group_info.comm, &proc_info->total_proc);
-
-	proc_info->len = get_proc_len(proc_info->rank, proc_info->total_proc, total_data_len);
-	proc_info->pre_len = get_proc_len(proc_info->rank - 1, proc_info->total_proc, total_data_len);
-	proc_info->next_len = get_proc_len(proc_info->rank + 1, proc_info->total_proc, total_data_len);
-
-	proc_info->first_index = proc_info->len * proc_info->rank;
-	if (proc_info->rank >= total_data_len % proc_info->total_proc) {
-		proc_info->first_index += total_data_len % proc_info->total_proc;
-	}
-	proc_info->last_index = proc_info->first_index + proc_info->len - 1;
+	info->last_index = info->first_index + info->len - 1;
 	
-	proc_info->data = (float *)malloc(sizeof(float) * proc_info->len);
-	return;
+	info->data = (float *)malloc(sizeof(float) * info->len);
+	
+	if (total_data_len < total_proc) {
+		info->total_proc = total_data_len;
+	}
+
+	if (info->len == 0) {
+		info->valid = 0;
+	} else {
+		info->valid = 1;
+	}
 }
 
-void free_proc_info(Proc_info *proc_info) {
-	free(proc_info->data);
+void free_proc_info(proc_info *info) {
+	free(info->data);
 }
 
 void print_array(float *arr, int len, int rank) {
@@ -97,10 +66,9 @@ void print_array(float *arr, int len, int rank) {
 	printf("\n");
 }
 
-// done return 1. continue return 0.
-int is_sort_done(Proc_info *proc_info, unsigned long *is_changed, unsigned long *is_continue, int &double_check) {
+int is_sort_done(unsigned long *is_changed, unsigned long *is_continue, int &double_check) {
 	int rc;
-	rc = MPI_Allreduce(is_changed, is_continue, 1, MPI_UNSIGNED_LONG, MPI_BOR, proc_info->group_info.comm);
+	rc = MPI_Allreduce(is_changed, is_continue, 1, MPI_UNSIGNED_LONG, MPI_BOR, MPI_COMM_WORLD);
 	if (rc != MPI_SUCCESS) printf("error: allreduce error.\n");
 	if (*is_continue == 0) {
 		if (double_check) {
@@ -153,59 +121,67 @@ unsigned long cmp_and_swap(float *local_data, float *remote_data, int local_len,
 	}
 }
 
-void odd_even_sort(Proc_info *proc_info) {
+void odd_even_sort(proc_info *info) {
 	int rc, double_check = 0;
 	unsigned long is_continue = 0, is_changed = 0;
 
-	float *pre_buffer, *next_buffer;
-	pre_buffer = (float *)malloc(sizeof(float) * proc_info->pre_len);
-	next_buffer = (float *)malloc(sizeof(float) * proc_info->next_len);
+	if (!info->valid) {
+		while (!is_sort_done(&is_changed, &is_continue, double_check)) {
+			is_continue = 0;
+		}
+		return;
+	}
 
-	std::sort(proc_info->data, proc_info->data + proc_info->len);
+	float *pre_buffer, *next_buffer;
+	pre_buffer = (float *)malloc(sizeof(float) * info->pre_len);
+	next_buffer = (float *)malloc(sizeof(float) * info->next_len);
+
+	std::sort(info->data, info->data + info->len);
 
 	for (int round = 0;; round++) {
 		int odd_or_even_phase = round % 2;		// 1 is odd. 0 is even
 
 		MPI_Request send_requset;
 		MPI_Status send_status, recv_status;
-		if (!(odd_or_even_phase ^ (proc_info->rank % 2))) {
+		if (!(odd_or_even_phase ^ (info->rank % 2))) {
 			// send to next process
-			if (proc_info->rank != proc_info->total_proc - 1) {
-				if (is_changed || (is_continue & 1UL << (proc_info->rank + 1)) || round < 2) {
-					rc = MPI_Isend(proc_info->data, proc_info->len, MPI_FLOAT, proc_info->rank + 1, 0, proc_info->group_info.comm, &send_requset);
-					if (rc != MPI_SUCCESS) printf("error: send; rank: %d, round: %d.\n", proc_info->rank, round);
+			if (info->rank != info->total_proc - 1) {
+				if (is_changed || (is_continue & 1UL << (info->rank + 1)) || round < 2) {
+					rc = MPI_Isend(info->data, info->len, MPI_FLOAT, info->rank + 1, 0, MPI_COMM_WORLD, &send_requset);
+					if (rc != MPI_SUCCESS) printf("error: send; rank: %d, round: %d.\n", info->rank, round);
 
-					rc = MPI_Recv(next_buffer, proc_info->next_len, MPI_FLOAT, proc_info->rank + 1, MPI_ANY_TAG, proc_info->group_info.comm, &recv_status);
-					if (rc != MPI_SUCCESS) printf("error: recv; rank: %d, round: %d.\n", proc_info->rank, round);
+					rc = MPI_Recv(next_buffer, info->next_len, MPI_FLOAT, info->rank + 1, MPI_ANY_TAG, MPI_COMM_WORLD, &recv_status);
+					if (rc != MPI_SUCCESS) printf("error: recv; rank: %d, round: %d.\n", info->rank, round);
 
 					rc = MPI_Wait(&send_requset, &send_status);
-					if (rc != MPI_SUCCESS) printf("error: wait send; rank: %d, round: %d.\n", proc_info->rank, round);
+					if (rc != MPI_SUCCESS) printf("error: wait send; rank: %d, round: %d.\n", info->rank, round);
 
-					is_changed = cmp_and_swap(proc_info->data, next_buffer, proc_info->len, proc_info->next_len, 1, proc_info->rank);
-					is_changed = is_changed << proc_info->rank;
+					is_changed = cmp_and_swap(info->data, next_buffer, info->len, info->next_len, 1, info->rank);
+					is_changed = is_changed << info->rank;
 				}
 			}
 		} else {
 			// send to previous process
-			if (proc_info->rank != 0) {
-				if (is_changed || (is_continue & 1UL << (proc_info->rank - 1)) || round < 2) {
-					rc = MPI_Isend(proc_info->data, proc_info->len, MPI_FLOAT, proc_info->rank - 1, 0, proc_info->group_info.comm, &send_requset);
-					if (rc != MPI_SUCCESS) printf("error: send; rank: %d, round: %d.\n", proc_info->rank, round);
+			if (info->rank != 0) {
+				if (is_changed || (is_continue & 1UL << (info->rank - 1)) || round < 2) {
+					rc = MPI_Isend(info->data, info->len, MPI_FLOAT, info->rank - 1, 0, MPI_COMM_WORLD, &send_requset);
+					if (rc != MPI_SUCCESS) printf("error: send; rank: %d, round: %d.\n", info->rank, round);
 					
-					rc = MPI_Recv(pre_buffer, proc_info->pre_len, MPI_FLOAT, proc_info->rank - 1, MPI_ANY_TAG, proc_info->group_info.comm, &recv_status);
-					if (rc != MPI_SUCCESS) printf("error: recv; rank: %d, round: %d.\n", proc_info->rank, round);
+					rc = MPI_Recv(pre_buffer, info->pre_len, MPI_FLOAT, info->rank - 1, MPI_ANY_TAG, MPI_COMM_WORLD, &recv_status);
+					if (rc != MPI_SUCCESS) printf("error: recv; rank: %d, round: %d.\n", info->rank, round);
 					
 					rc = MPI_Wait(&send_requset, &send_status);
-					if (rc != MPI_SUCCESS) printf("error: wait send; rank: %d, round: %d.\n", proc_info->rank, round);
+					if (rc != MPI_SUCCESS) printf("error: wait send; rank: %d, round: %d.\n", info->rank, round);
 
-					is_changed = cmp_and_swap(proc_info->data, pre_buffer, proc_info->len, proc_info->pre_len, 0, proc_info->rank);
-					is_changed = is_changed << proc_info->rank;
+					is_changed = cmp_and_swap(info->data, pre_buffer, info->len, info->pre_len, 0, info->rank);
+					is_changed = is_changed << info->rank;
 				}
 			}
 		}
-
+		
+		// check if it's done
 		is_continue = 0;	// reset check point.
-		if (is_sort_done(proc_info, &is_changed, &is_continue, double_check)) {
+		if (is_sort_done(&is_changed, &is_continue, double_check)) {
 			break;
 		}
 	}
@@ -220,40 +196,38 @@ int main(int argc, char** argv) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	
-	Proc_info proc_info;
+	proc_info info;
 
-	// recreate group
-	if (init_group_info(&proc_info, rank, size, atoi(argv[1]))) {
-		MPI_Finalize();
-		return 0;
-	}
-	init_proc_info(&proc_info, size, atoi(argv[1]));
+	init_proc_info(&info, rank, size, atoi(argv[1]));
 
 	// open and read
 	MPI_File f_in;
-	rc = MPI_File_open(proc_info.group_info.comm, argv[2], MPI_MODE_RDONLY, MPI_INFO_NULL, &f_in);
+	rc = MPI_File_open(MPI_COMM_WORLD, argv[2], MPI_MODE_RDONLY, MPI_INFO_NULL, &f_in);
 	if (rc != MPI_SUCCESS)	printf("error: open read file.\n");
 
-	rc = MPI_File_read_at(f_in, sizeof(float) * proc_info.first_index, proc_info.data, proc_info.len, MPI_FLOAT, MPI_STATUS_IGNORE);
-	if (rc != MPI_SUCCESS) printf("error: read file.\n");
+	if (info.valid) {
+		rc = MPI_File_read_at(f_in, sizeof(float) * info.first_index, info.data, info.len, MPI_FLOAT, MPI_STATUS_IGNORE);
+		if (rc != MPI_SUCCESS) printf("error: read file.\n");
+	}
 
 	rc = MPI_File_close(&f_in);
 	if (rc != MPI_SUCCESS) printf("error: close read file.\n");
 	
-	odd_even_sort(&proc_info);
+	odd_even_sort(&info);
 
 	// open and write
 	MPI_File f_out;
-	rc = MPI_File_open(proc_info.group_info.comm, argv[3], MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &f_out);
+	rc = MPI_File_open(MPI_COMM_WORLD, argv[3], MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &f_out);
 	if (rc != MPI_SUCCESS)	printf("error: open write file.\n");
 
-	rc = MPI_File_write_at(f_out, sizeof(float) * proc_info.first_index, proc_info.data, proc_info.len, MPI_FLOAT, MPI_STATUS_IGNORE);
-	if (rc != MPI_SUCCESS) printf("error: write file.\n");
-
+	if (info.valid) {
+		rc = MPI_File_write_at(f_out, sizeof(float) * info.first_index, info.data, info.len, MPI_FLOAT, MPI_STATUS_IGNORE);
+		if (rc != MPI_SUCCESS) printf("error: write file.\n");
+	}
 	MPI_File_close(&f_out);
 	if (rc != MPI_SUCCESS) printf("error: close write file.\n");
 
-	free_proc_info(&proc_info);
+	free_proc_info(&info);
 	MPI_Finalize();
 	return 0;
 }
